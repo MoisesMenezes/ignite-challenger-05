26869de1700fcc42fea7b0ddbcaa441cfa0d7e52
Moises Menezes <moisesmenezes90@gmail.com>
2021-05-12 21:45:14 -0300

    feat: add global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,9 +24,13 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <h1>OLA MUNDO</h1>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -26,9 +26,11 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post() {
+  return (
+    <h1>OLA MUNDO POST</h1>
+  )
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient();
