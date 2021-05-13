7e42fe30496137a21682e84e8ff83acac4b73fff
Moises Menezes <moisesmenezes90@gmail.com>
2021-05-12 23:33:56 -0300

    feat: add home page with static posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
+import { FiUser, FiCalendar } from 'react-icons/fi';
+import Prismic from "@prismicio/client";
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -27,14 +29,71 @@ interface HomeProps {
 export default function Home() {
   return (
     <>
-      <h1>OLA MUNDO</h1>
+      <header className={styles.postHeader}>
+        <img src="/logo.svg" alt="logo" />
+      </header>
+      <main className={commonStyles.contentContainer}>
+        <div className={styles.postContainer}>
+          <div className={styles.postContent}>
+            <h1>Como utilizar Hooks</h1>
+            <p>Pensando em sincronização em vez de clicos de vida</p>
+
+            <div className={styles.iconsContainer}>
+              <div className={styles.iconsContent}>
+                <FiCalendar size={20} />
+                <p>15 Mar 2021</p>
+              </div>
+              <div className={styles.iconsContent}>
+                <FiUser size={20} />
+                <p>Joseph Oliveira</p>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.postContent}>
+            <h1>Como utilizar Hooks</h1>
+            <p>Pensando em sincronização em vez de clicos de vida</p>
+
+            <div className={styles.iconsContainer}>
+              <div className={styles.iconsContent}>
+                <FiCalendar size={20} />
+                <p>15 Mar 2021</p>
+              </div>
+              <div className={styles.iconsContent}>
+                <FiUser size={20} />
+                <p>Joseph Oliveira</p>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.postContent}>
+            <h1>Como utilizar Hooks</h1>
+            <p>Pensando em sincronização em vez de clicos de vida</p>
+
+            <div className={styles.iconsContainer}>
+              <div className={styles.iconsContent}>
+                <FiCalendar size={20} />
+                <p>15 Mar 2021</p>
+              </div>
+              <div className={styles.iconsContent}>
+                <FiUser size={20} />
+                <p>Joseph Oliveira</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+// export const getStaticProps: GetStaticProps = async () => {
+//   const prismic = getPrismicClient();
+//   // const postsResponse = await prismic.query(
+//   //   [Prismic.predicates]
+//   // );
 
-//   // TODO
+//   return {
+//     props: {}
+//   }
 // };
